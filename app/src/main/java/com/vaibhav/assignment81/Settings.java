a52ea5ff02de4b843fa63fb953dbd707463e7a92
Vaibhav Gupta <guptvaibhav.gupta@gmail.com>
2017-02-10 23:54:08 +0530

    Initial commit

---
+++ app/src/main/java/com/vaibhav/assignment81/Settings.java
@@ -0,0 +1,16 @@
+package com.vaibhav.assignment81;
+
+
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+
+
+public class Settings extends PreferenceActivity {
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState)    {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.user_settings);
+    }
+
+}
